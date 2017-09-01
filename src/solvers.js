c4c9b0d1e7fb0a8808a1ff6bb1e54b34f71d15ed
Hack Reactor Student <student@hackreactor.com>
2017-08-31 18:55:09 -0700

    Complete queen count functionality

--- src/solvers.js
+++ src/solvers.js
@@ -110,7 +110,40 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  var board = new Board({n: n});
+  if (n === 0) {
+    return 1;
+  }
+  var buildSolution = function(piecesRemaining) {
+    if (!piecesRemaining) {
+      solutionCount++;
+      return;
+    }
+    var rowIndex = n - piecesRemaining;
+    for (var colIndex = 0; colIndex < n; colIndex++) { 
+      board.togglePiece(rowIndex, colIndex);
+      if (board.hasAnyQueensConflicts()) {
+        if (colIndex === n - 1) {
+          board.togglePiece(rowIndex, colIndex);
+          break;
+        }
+        board.togglePiece(rowIndex, colIndex);
+      } else {
+        
+        buildSolution(piecesRemaining - 1); 
+        
+        board.togglePiece(rowIndex, colIndex);
+      }
+    }
+    
+  };
+  for (var i = 0; i < n; i++) {
+    board = new Board({n: n});
+    board.togglePiece(0, i);
+    buildSolution(n - 1);
+  }
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
